25c2a9f8835e6d2d226abbf2c65dc6f102500223
Alex Moran <kj4cgv@gmail.com>
2017-09-11 11:33:23 -0700

    Add files via upload

---
+++ Labels/A6467.cs
@@ -0,0 +1,24 @@
+﻿namespace Coeus.Infrastructure.SharpPDFLabel.Labels {    
+    /// <summary>
+	/// Dimensions: 0.5in x 1.75in
+	/// Per Sheet: 80 per sheet 
+	/// Inkjet code: ?
+	/// </summary>
+    public class A6467 : Label {
+        public A6467() {
+            height = 0.5;
+            width = 1.75;            
+            gapWidth = 0.3;
+            gapHeight = 0;
+
+            pageMarginTop = 0.5;
+            pageMarginBottom = 0.5;
+            pageMarginLeft = 0.3;
+            pageMarginRight = 0.3;
+
+            PageSize = PaperSize.LETTER;
+            columns = 4;
+            rows = 20;
+        }
+    }
+}
---
+++ Labels/L7160.cs
@@ -0,0 +1,24 @@
+﻿namespace Coeus.Infrastructure.SharpPDFLabel.Labels {
+    /// <summary>
+    /// Dimensions: 63.5mm x 38.1mm 
+    /// Per Sheet: 21 per sheet 
+    /// Inkjet code: J8160
+    /// </summary>
+    public class L7160 : Label {
+        public L7160() {
+            width = 63.5 / 25.4;
+            height = 38.1 / 25.4;
+            gapWidth = 2.5 / 25.4;
+            gapHeight = 0;
+
+            pageMarginTop = 15.1 / 25.4;
+            pageMarginBottom = 15.1 / 25.4;
+            pageMarginLeft = 7.2 / 25.4;
+            pageMarginRight = 7.2 / 25.4;
+
+            PageSize = PaperSize.A4;
+            columns = 3;
+            rows = 7;
+        }
+    }
+}
